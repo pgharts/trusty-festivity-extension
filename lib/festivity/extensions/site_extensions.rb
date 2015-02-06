4a2fe77921abe7145f7a9beb7bede9435f9fac39
Eric Sipple <saalon@gmail.com>
2015-02-06 14:23:34 -0500

    Now pulling only active category types.

--- lib/festivity/extensions/site_extensions.rb
+++ lib/festivity/extensions/site_extensions.rb
@@ -6,6 +6,14 @@ module Festivity
         base.class_eval {
           has_many :festivity_category_types
         }
+        include Festivity::Extensions::SiteExtensions::SiteMethods
+      end
+
+      module SiteMethods
+
+        def festivity_active_category_types
+          self.festivity_category_types.where(["inactive = false"])
+        end
 
       end
 
