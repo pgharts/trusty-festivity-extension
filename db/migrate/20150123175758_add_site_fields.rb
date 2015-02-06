383813d4703b58528bb089855fc6756d0d9bb20f
Eric Sipple <saalon@gmail.com>
2015-02-06 08:15:38 -0500

    Updating site fields.

--- db/migrate/20150123175758_add_site_fields.rb
+++ db/migrate/20150123175758_add_site_fields.rb
@@ -1,11 +1,15 @@
 class AddSiteFields < ActiveRecord::Migration
   def up
-    add_column :sites, :box_office_phone_number, :string
-    add_column :sites, :google_search_api_key, :string
+    add_column :sites, :festivity_start_date, :date
+    add_column :sites, :festivity_end_date, :date
+    add_column :sites, :festivity_festival_name, :string
+    add_column :sites, :festivity_google_api_key, :string
   end
 
   def down
-    remove_column :sites, :box_office_phone_number
-    remove_column :sites, :google_search_api_key
+    remove_column :sites, :festivity_start_date
+    remove_column :sites, :festivity_end_date
+    remove_column :sites, :festivity_festival_name
+    remove_column :sites, :festivity_google_api_key
   end
 end
