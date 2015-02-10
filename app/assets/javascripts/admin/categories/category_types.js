e925001296da9bcf13d54c3655cde093d021f0b7
Eric Sipple <saalon@gmail.com>
2015-02-10 15:10:47 -0500

    Hacking out javascript for type creation.

--- app/assets/javascripts/admin/categories/category_types.js
+++ app/assets/javascripts/admin/categories/category_types.js
@@ -2,4 +2,16 @@ $(function () {
   $('button.popup').each(function(){
     Popup.setup(this);
   });
+
+  $('#add_type_button').click(function(e){
+    e.preventDefault();
+    $.post(
+        $("#new_festivity_category_type").attr('action'),
+        {
+          name: $("#type_name_field").val(),
+          site_id: $("#type_site_id").val(),
+          page_class: $("#festivity_category_type_page_class").val()
+        });
+  });
+
 });
