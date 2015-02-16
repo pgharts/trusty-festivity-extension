d27655fcae7ebd1fbdb4bab624bafdc5d66a94b8
Eric Sipple <saalon@gmail.com>
2015-02-16 15:38:46 -0500

    Updating column name for linking categories to types.

--- db/migrate/20150204113427832_add_categories_table.rb
+++ db/migrate/20150204113427832_add_categories_table.rb
@@ -2,7 +2,7 @@ class AddCategoriesTable < ActiveRecord::Migration
   def up
     create_table :festivity_categories do |t|
       t.string :name
-      t.integer :category_type_id
+      t.integer :festivity_category_type_id
       t.integer :parent_id
       t.integer :position
       t.boolean :inactive, default: false
