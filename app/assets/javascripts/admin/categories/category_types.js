2f33d085e8d56a783624a19595022de2fa6f2d15
Eric Sipple <saalon@gmail.com>
2015-02-17 09:38:30 -0500

    Adding works, though none of the associated re-wiring of js events does yet.

--- app/assets/javascripts/admin/categories/category_types.js
+++ app/assets/javascripts/admin/categories/category_types.js
@@ -3,6 +3,11 @@ function updateCategoryTypeList(html) {
   attachListEvents();
 }
 
+function updateCategoryLists(html) {
+  $("#festivity-filters").replaceWith(html);
+  $('#festivity-filters li a:first').tab('show');
+}
+
 function attachListEvents() {
   $('a.festivity-type').click(function(e){
     e.preventDefault();
@@ -39,6 +44,30 @@ $(function () {
         updateCategoryTypeList(data);
         Popup.close();
         $("#type_name_field").val("");
+        $("#category-type-error").html("");
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        $("#category-type-error").html(jqXHR.responseText);
+      }
+    });
+
+  });
+
+  $('input[data-role="add_category"]').click(function(e){
+    e.preventDefault();
+    var category =  $(this).attr('data-category');
+    $.ajax({
+      url: $("#new_" + category).attr('action'),
+      type: 'POST',
+      data: {
+        name: $("#" + category + "_name_field").val(),
+        category_type_id: $("#" + category + "_type_id").val(),
+        parent_id: $("#" + category + "_parent_id").val()
+      },
+      success: function(data, textStatus, jqXHR){
+        updateCategoryLists(data);
+        Popup.close();
+        $("#" + category + "_name_field").val("");
         $("#category-type-error").html("");
       },
       error: function(jqXHR, textStatus, errorThrown){
--- app/models/festivity_category_type.rb
+++ app/models/festivity_category_type.rb
@@ -1,5 +1,6 @@
 class FestivityCategoryType < ActiveRecord::Base
   acts_as_list scope: :site_id
   has_many :festivity_categories
+  belongs_to :site
   validates_uniqueness_of :name, scope: :site_id
 end
