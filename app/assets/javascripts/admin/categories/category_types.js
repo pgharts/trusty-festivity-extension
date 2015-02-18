31411b3f1944bcc4ff89b19bd369b761bef5a5ee
Eric Sipple <saalon@gmail.com>
2015-02-18 13:58:07 -0500

    Fixed event binding on categories.

--- app/assets/javascripts/admin/categories/category_types.js
+++ app/assets/javascripts/admin/categories/category_types.js
@@ -36,7 +36,30 @@ function attachListEvents() {
 }
 
 function attachCategoryEvents(category) {
+  $('input[data-role="add_category"]').unbind("click");
+  $('input[data-role="add_category"]').click(function(e){
+    e.preventDefault();
+    var category =  $(this).attr('data-category');
+    $.ajax({
+      url: $("#new_" + category).attr('action'),
+      type: 'POST',
+      data: {
+        name: $("#" + category + "_name_field").val(),
+        category_type_id: $("#" + category + "_type_id").val(),
+        parent_id: $("#" + category + "_parent_id").val()
+      },
+      success: function(data, textStatus, jqXHR){
+        updateCategoryLists(data, category);
+        Popup.close();
+        $("#" + category + "_name_field").val("");
+        $("#category-type-error").html("");
+      },
+      error: function(jqXHR, textStatus, errorThrown){
+        $("#category-type-error").html(jqXHR.responseText);
+      }
+    });
 
+  });
   if(category == null) {
     $('#festivity-filters li a:first').tab('show');
   }
@@ -66,30 +89,6 @@ $(function () {
         updateCategoryTypeList(data);
         Popup.close();
         $("#type_name_field").val("");
-        $("#category-type-error").html("");
-      },
-      error: function(jqXHR, textStatus, errorThrown){
-        $("#category-type-error").html(jqXHR.responseText);
-      }
-    });
-
-  });
-
-  $('input[data-role="add_category"]').click(function(e){
-    e.preventDefault();
-    var category =  $(this).attr('data-category');
-    $.ajax({
-      url: $("#new_" + category).attr('action'),
-      type: 'POST',
-      data: {
-        name: $("#" + category + "_name_field").val(),
-        category_type_id: $("#" + category + "_type_id").val(),
-        parent_id: $("#" + category + "_parent_id").val()
-      },
-      success: function(data, textStatus, jqXHR){
-        updateCategoryLists(data, category);
-        Popup.close();
-        $("#" + category + "_name_field").val("");
         $("#category-type-error").html("");
       },
       error: function(jqXHR, textStatus, errorThrown){
