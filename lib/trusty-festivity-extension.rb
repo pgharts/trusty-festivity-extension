c69c2c8b9ca54b4064956e26985d6b68ab8aed31
Eric Sipple <saalon@gmail.com>
2015-03-13 12:48:47 -0400

    Removing debugging code.

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "0.0.3"
+  VERSION     = "0.0.4"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
