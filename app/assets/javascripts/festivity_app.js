575574575db3b898c6f440f8da8b1d36ee0e6835
Eric Sipple <saalon@gmail.com>
2015-03-13 14:10:23 -0400

    Fixing order of app js.

--- app/assets/javascripts/festivity_app.js
+++ app/assets/javascripts/festivity_app.js
@@ -1,8 +1,8 @@
 // Base and external Javascript libraries
 //= require lodash.min
-//= require bootstrap.min
 //= require jquery
 //= require jquery-migrate.js
+//= require bootstrap.min
 //= require readmore.min
 //= require underscore.js
 //= require jasny-bootstrap.js
