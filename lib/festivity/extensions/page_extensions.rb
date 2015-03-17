f2354275d9f3aa673715515b9d60c6b7f1bc3487
Eric Sipple <saalon@gmail.com>
2015-03-17 13:06:27 -0400

    Adding default page parts to event pages.

--- lib/festivity/extensions/page_extensions.rb
+++ lib/festivity/extensions/page_extensions.rb
@@ -4,6 +4,10 @@ module Festivity
 
       def self.included(base)
         base.class_eval {
+
+          before_save :set_page_defaults
+          after_create  :create_page_defaults
+
           has_many :festivity_category_types
           has_many :festivity_page_categories
           has_many :festivity_categories, through: :festivity_page_categories
@@ -14,6 +18,18 @@ module Festivity
       end
 
       module PageMethods
+
+        def create_page_defaults
+          self.save if festivity_base_page?
+        end
+
+        def set_page_defaults
+          if festivity_base_page? && self.id
+            self.slug = self.id unless self.slug == self.id
+            parts.create(:name => 'video_embed', :content => "") unless parts.any? { |part| part.name == 'video_embed' }
+            parts.create(:name => 'sponsor_logo', :content => "") unless parts.any? { |part| part.name == 'sponsor_logo' }
+          end
+        end
 
         def image
           image_url('featured_image')
@@ -27,6 +43,12 @@ module Festivity
             page_parent = page_parent.parent
           end
           page_organization
+        end
+
+        private
+
+        def festivity_base_page?
+          (self.class_name == "FestivityEventPage") | (self.class_name == "FestivityMarketPage")
         end
 
       end
