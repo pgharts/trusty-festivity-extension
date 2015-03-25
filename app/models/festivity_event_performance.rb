00500c57d6de97bb13106e0a064ef791b1a08307
Eric Sipple <saalon@gmail.com>
2015-03-25 15:04:43 -0400

    Fixed association between event performances and categories.

--- app/models/festivity_event_performance.rb
+++ app/models/festivity_event_performance.rb
@@ -2,6 +2,7 @@ class FestivityEventPerformance < ActiveRecord::Base
   self.table_name = 'festivity_event_performances'
   after_initialize :readonly!
 
-  has_many :festivity_categories, through: :festivity_page_categories, foreign_key: :event_id, primary_key: :event_id
+  has_many :festivity_page_categories, foreign_key: :page_id, primary_key: :event_id
+  has_many :festivity_categories, through: :festivity_page_categories
 
 end
