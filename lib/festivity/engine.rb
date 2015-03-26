591648093588899806227af1a33f77f349e35e83
Patrick Fitzgerald <fitzgerald@trustarts.org>
2015-03-26 15:12:13 -0400

    styling pages

--- lib/festivity/engine.rb
+++ lib/festivity/engine.rb
@@ -3,7 +3,7 @@ module Festivity
     paths["app/helpers"] = []
 
     initializer "trusty_cms.assets.precompile" do |app|
-      app.config.assets.precompile += %w(admin/festivity_admin.css admin/festivity_admin.js festivity.css festivity_app.js)
+      app.config.assets.precompile += %w(admin/festivity_admin.css admin/festivity_admin.js festivity.css festivity_app.js skins/pghkids/skin.css)
     end
   end
 end
