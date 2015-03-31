8c2bd671b8be64edec4c1619c617ee1ea8a26d47
Eric Sipple <saalon@gmail.com>
2015-03-31 11:02:23 -0400

    Query URL building complete.

--- app/assets/javascripts/events/filters.js
+++ app/assets/javascripts/events/filters.js
@@ -12,7 +12,29 @@ Festivity.Filters = {
   },
 
   queryEvents: function() {
-    alert("Clicked!");
+
+    alert(Festivity.Filters.eventsUrl());
+  },
+
+  eventsUrl: function() {
+    var dates = _.map($(".date-filters input[type='checkbox']:checked"), Festivity.Filters.checkboxValues);
+    var categories = _.map($(".category-filters input[type='checkbox']:checked"), Festivity.Filters.checkboxValues);
+    var sort_string = "?sort=" + $("#sort").val();
+    var params_string = Festivity.Filters.arrayToParams("dates", dates) + Festivity.Filters.arrayToParams("categories", categories);
+    return $("#events-url").val() + sort_string + params_string;
+  },
+
+  checkboxValues: function(checkbox) {
+    return $(checkbox).val();
+  },
+
+  arrayToParams: function(param, array) {
+    if (array.length > 0) {
+      return "&" + param + "=" + array.join(",");
+    }
+    else {
+      return "";
+    }
   }
 
 };
