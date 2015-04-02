0509ae839d21adccfe50da85dfc46f892086405d
Eric Sipple <saalon@gmail.com>
2015-04-02 08:44:25 -0400

    Building out search criteria.

--- app/models/festivity_event_list.rb
+++ app/models/festivity_event_list.rb
@@ -10,22 +10,45 @@ class FestivityEventList
   end
 
   def self.search(criteria, order_by = "start_date")
-    # find events WHERE site_id = current site
-    # JOIN Page Categories; Where category_id in [list]
-    # JOIN Performances on event_id
+    where_clause = parse_criteria(criteria)
+    # where in event ids
     FestivityEventList.new(
         FestivityEventList::FestivityEventPerformance.
             includes(:assets).
             joins(:festivity_categories).
-            where("festivity_categories.id" => [1]).
+            where(where_clause).
             preload(:festivity_categories)
     )
   end
 
   private
 
-  def parse_criteria(criteria)
+  # Return a list of unique event ids that match the provided dates
+  def self.event_ids_for_dates(dates)
+    FestivityEventList::FestivityEventPerformance.where(date_criteria(dates)).map {|e| e.event_id}.uniq
+  end
 
+  def self.date_criteria(dates_string)
+    date_queries = dates_string.split(',').map do |date_string|
+      start_date = DateTime.parse(dates_string)
+      end_date = start_date.advance(hours: 23, minutes: 59)
+      <<-SQL
+        (
+          (start_date >= '#{start_date}' AND start_date <= '#{end_date}')
+          OR
+          (end_date >= '#{start_date}' AND end_date <= '#{end_date}')
+        )
+      SQL
+    end
+    date_queries.join(" OR ")
+  end
+
+  def self.parse_criteria(criteria)
+    where_clause = {}
+    event_ids = event_ids_for_dates(criteria[:dates]) if criteria[:dates]
+    where_clause["event_id"] = event_ids if event_ids
+    where_clause["festivity_categories.id"] = criteria[:categories].split(",") if criteria[:categories]
+    where_clause
   end
 
 
