430a8a08c8616c1dcd721bef144097ff02d4281e
Eric Sipple <saalon@gmail.com>
2015-04-06 11:56:58 -0400

    Setting up location page.

--- app/controllers/festivity_locations_controller.rb
+++ app/controllers/festivity_locations_controller.rb
@@ -1,6 +1,6 @@
 class FestivityLocationsController < ApplicationController
   no_login_required
-  trusty_layout "normal"
+  trusty_layout "base"
 
   def show
     @location = FestivityLocationPage.find_by_slug_for_site(params[:id]).first
