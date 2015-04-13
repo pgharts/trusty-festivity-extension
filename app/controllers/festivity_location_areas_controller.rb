9643a2fb9909c19f2a93034440cb44f50a44074b
Eric Sipple <saalon@gmail.com>
2015-04-13 09:44:55 -0400

    Fixing indentation in location areas controller.

--- app/controllers/festivity_location_areas_controller.rb
+++ app/controllers/festivity_location_areas_controller.rb
@@ -7,7 +7,7 @@ class FestivityLocationAreasController < ApplicationController
     @area = FestivityLocationAreaPage.find_by_slug_for_site(params[:id]).first
 
     if @area
-    @title = "#{current_site.festivity_festival_name}: #{@area.title}"
+      @title = "#{current_site.festivity_festival_name}: #{@area.title}"
     else
       file_not_found_for_site
     end
