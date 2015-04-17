e592488e97a80d038b0955028834e800cde24a9d
Eric Sipple <saalon@gmail.com>
2015-04-17 07:24:33 -0400

    Fixing button links for IE compatibility.

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "0.2.3"
+  VERSION     = "0.2.4"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
