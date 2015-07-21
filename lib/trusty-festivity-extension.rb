d704b8763bfe7723f9610b57d0ecd11eed49e326
Eric Sipple <saalon@gmail.com>
2015-07-21 12:27:02 -0400

    Updating to 1.0.0 for Rails 4 work.

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "0.2.6"
+  VERSION     = "1.0.0"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
