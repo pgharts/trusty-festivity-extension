84997244f9158d22080cac9d9c23b730c5b1a1b5
Eric Sipple <esipple@stitchfix.com>
2015-11-28 13:14:59 -0500

    2.3.0

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "2.2.9"
+  VERSION     = "2.3.0"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
