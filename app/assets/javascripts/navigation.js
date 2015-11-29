5d664e45071e639f2e15a9ae2072778837f3afe7
Eric Sipple <esipple@stitchfix.com>
2015-11-29 13:42:27 -0500

    Reworked navigation javascript to actually work.

--- app/assets/javascripts/navigation.js
+++ app/assets/javascripts/navigation.js
@@ -1,10 +1,9 @@
-var $menuTrigger = $('.menu-trigger'),
-    $navWrapper = $('.nav-wrapper'),
-    $primaryMenu = $('.primary-navigation'),
-    $utilityMenu = $('.utility-navigation'),
-    $iconMenuButton = $('.iconmenu-button');
 
 function toggleNavWrapper() {
+  var $menuTrigger = $('.menu-trigger'),
+    $navWrapper = $('.nav-wrapper'),
+    $iconMenuButton = $('.iconmenu-button');
+  
   $menuTrigger.on("click", function(e) {
     $menuTrigger.toggleClass('is-active');
     $iconMenuButton.toggleClass('open');
@@ -12,24 +11,32 @@ function toggleNavWrapper() {
   });
 };
 
+$(document).ready(function(){
+  var $menuTrigger = $('.menu-trigger'),
+    $navWrapper = $('.nav-wrapper'),
+    $primaryMenu = $('.primary-navigation'),
+    $utilityMenu = $('.utility-navigation'),
+    $iconMenuButton = $('.iconmenu-button');
 // Fixes broken navigation when changing states on mobile then expanding to desktop
-jRes.addFunc({
-  breakpoint: 'nav',
-  enter: function() {
-    $primaryClone = $primaryMenu.clone().addClass('clone');
-    $primaryMenu.hide();
-    $primaryClone.insertBefore($utilityMenu);
-  },
-  exit: function(){
-    $navWrapper.removeClass('is-visible').removeAttr('style');
-    $menuTrigger.removeClass('is-active');
-    $iconMenuButton.removeClass('open');
-    $primaryClone.remove();
+  jRes.addFunc({
+    breakpoint: 'nav',
+    enter: function() {
+      $primaryClone = $primaryMenu.clone().addClass('clone');
+      $primaryMenu.hide();
+      $primaryClone.insertBefore($utilityMenu);
+    },
+    exit: function(){
+      $navWrapper.removeClass('is-visible').removeAttr('style');
+      $menuTrigger.removeClass('is-active');
+      $iconMenuButton.removeClass('open');
+      $primaryClone.remove();
 
-    if (!$primaryMenu.is(':visible')) {
-      $primaryMenu.show();
+      if (!$primaryMenu.is(':visible')) {
+        $primaryMenu.show();
+      }
     }
-  }
+  });
+  toggleNavWrapper();
 });
 
-toggleNavWrapper();
+
