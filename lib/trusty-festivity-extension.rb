32010f6a3f6a1081c4fcd08c90c9ea71d0bdec08
Eric Sipple <esipple@stitchfix.com>
2015-11-29 14:32:48 -0500

    What was that g at the end of the version? Bumping to 2.3.12.

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "2.3.11g"
+  VERSION     = "2.3.12"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
