264dac3992204368728ceac71773e306aca61b77
danigirl329 <greaves@trustarts.org>
2016-03-30 11:30:37 -0400

    BugzID: 2666 - Implements Rad Social popup on event page
    On the event detail page, there is now a share this button at the bottom of the description and when clicked, it pops up a social share modal.
    
    I included the base social media icons (minus g+)
    
    I am waiting on the proper copy of what the social icons should say, but the basic functionality is there.

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,15 +1,16 @@
 $(document).ready(function(){
   function showModal() {
     $('.modal-link').on("click", function(e) {
-      $(this).next('.modal').addClass('is-visible');
+      e.preventDefault();
+      $('.modal').addClass('is-visible');
     });
-  };
+  }
 
   function hideModal() {
     $('.modal .close-btn, .modal .close-link, .modal .add-to-cart').on("click", function(e) {
       e.preventDefault();
       $('.modal').removeClass('is-visible');
-    })
+    });
   }
 
   showModal();
--- app/helpers/festivity_events_helper.rb
+++ app/helpers/festivity_events_helper.rb
@@ -9,4 +9,20 @@ module FestivityEventsHelper
     performances.map {|perf| "<li>#{perf.start_date.strftime('%A, %B %d')}, #{perf.start_date.strftime('%I:%M %p').downcase} - #{perf.end_date.strftime('%I:%M %p').downcase}</li>" }.join("")
   end
 
+  def social_share_message(production)
+    "Check out #{production.title}!"
+  end
+
+  def social_share_email_subject(production)
+    "Check out #{production.title}"
+  end
+
+  def social_share_email_message(production)
+    "I thought you might be interested in seeing #{production.title}."
+  end
+
+  def shared_production_url(production)
+    "#{event_url}?cid=CDSocial"
+  end
+
 end
