b98cb627ec8869026554a3ea0bf1333e31353376
Eric Sipple <esipple@stitchfix.com>
2016-04-02 10:39:49 -0400

    Restricted location page events to the festival dates.

--- app/controllers/festivity_locations_controller.rb
+++ app/controllers/festivity_locations_controller.rb
@@ -7,7 +7,7 @@ class FestivityLocationsController < ApplicationController
     @location = FestivityLocationPage.find_by_slug_for_site(params[:id]).first
     if @location
       @location_events = @title = Rails.cache.fetch("location-#{@location.slug}", expires_in: 2.hours) do
-        FestivityEventList.find_by_location(@location.id)
+        FestivityEventList.find_by_location(@location.id, current_site)
       end
       @title = "#{current_site.festivity_festival_name}: #{@location.title}"
     else
