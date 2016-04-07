897e08bff162540dd5a8526ce3989a0bb656a0d2
danigirl329 <greaves@trustarts.org>
2016-04-07 12:53:13 -0400

    BugzID: 3316 - Fixed the modal 404 and fixed some bugs within
    Removed the modal js call from the view
    
    Updated the view to put the class on the list so that it finds the next modal window. This will now open modals independently of each other.
    
    Updated the js to grab the next modal window instead of just the modal class.

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -2,12 +2,12 @@ $(document).ready(function(){
   function showModal() {
     $('.modal-link').on("click", function(e) {
       e.preventDefault();
-      $('.modal').addClass('is-visible');
+      $(this).next('.modal').addClass('is-visible');
     });
   }
 
   function hideModal() {
-    $('.modal .close-btn, .modal .close-link, .modal .add-to-cart').on("click", function(e) {
+    $('.modal .close-btn, .modal .close-link').on("click", function(e) {
       e.preventDefault();
       $('.modal').removeClass('is-visible');
     });
