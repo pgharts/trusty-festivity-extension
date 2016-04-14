e2cbfef4ca457d64d3864e2fbcee58db04b08736
Brittany Martin <brittany.jill.martin@gmail.com>
2016-04-14 07:38:21 -0400

    Registering version 2.5.3

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,8 +1,8 @@
 module TrustyFestivityExtension
-  VERSION     = "2.5.2"
+  VERSION     = "2.5.3"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
-  AUTHORS     = ["Eric Sipple, Danielle Greaves, Brittany Martin"]
-  EMAIL       = ["sipple@trustarts.org"]
+  AUTHORS     = ["Eric Sipple, Danielle Greaves, Brittany Martin, Patrick FitzGerald"]
+  EMAIL       = ["fitzgerald@trustarts.org"]
 end
