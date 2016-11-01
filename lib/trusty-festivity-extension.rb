d7b5f0b44046a28cd7353169fc5e4f64e2e8a74a
Brittany Martin <brittany.jill.martin@gmail.com>
2016-11-01 13:15:39 -0400

    Register Festivity Extension - 2.5.17

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "2.5.16"
+  VERSION     = "2.5.17"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
