edf7727f3a315e06111103e7c2a60c5d211f5757
Danielle Greaves <greaves@trustarts.org>
2016-11-29 11:27:27 -0500

    Updated version and bundle install

--- lib/trusty-festivity-extension.rb
+++ lib/trusty-festivity-extension.rb
@@ -1,5 +1,5 @@
 module TrustyFestivityExtension
-  VERSION     = "2.5.19"
+  VERSION     = "2.6"
   SUMMARY     = "Festival microsite engine for Trusty CMS"
   DESCRIPTION = "Event management for arts festivals."
   URL         = "http://github.com/pgharts/trusty-festivity-extension"
